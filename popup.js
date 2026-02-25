3715d678a86533ec060e45e4e3774f87cadf97d8
enliven <cankat.17.polat@gmail.com>
2026-02-26 01:06:36 +0300

    ui fix

--- popup.js
+++ popup.js
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function renderError(msg) {
     resultsDiv.classList.remove('hidden');
-    resultsDiv.innerHTML = `<span class="error"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg> ${msg}</span>`;
+    resultsDiv.innerHTML = `<span class="error">${msg}</span>`;
   }
 
   async function githubSearchCode(query) {
@@ -104,7 +104,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function displayResults(items, totalCount, label) {
     resultsDiv.classList.remove('hidden');
     if (!items || items.length === 0) {
-      resultsDiv.innerHTML = `<span class="success"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg> No exposures found for ${label}!</span>`;
+      resultsDiv.innerHTML = `<span class="success">No exposures found for ${label}</span>`;
       return;
     }
 
